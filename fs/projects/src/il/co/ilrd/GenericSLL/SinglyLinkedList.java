6f48cadd2d9ad15dbd3c74684255e0c0bee51487
itai.marienberg <itaim88@gmail.com>
2020-04-02 18:15:46 +0300

    GenericSLL

--- fs/projects/src/il/co/ilrd/GenericSLL/SinglyLinkedList.java
+++ fs/projects/src/il/co/ilrd/GenericSLL/SinglyLinkedList.java
@@ -1,81 +1,154 @@
 package il.co.ilrd.GenericSLL;
 
-import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.List;
 
 public class SinglyLinkedList<T> implements Iterable<T> {
-	private Node head;
+	private Node<T> head = new Node<>(null ,null);
+	private volatile int listCounter = 0;
 	
-	private class Node {
-		
-		private T data;
-		private Node nextNode;
+	private static class Node<T>  {
+	private T data;
+	private Node<T> nextNode;
 
-		private Node(T data, Node nextNode) {
-			
-		}
-		public T getData() {
-			return data;
-		}
-		public void setNext(Node next) {
-			
-		}
-		public Node getNext(Node next) {
-			return null;
-		}
+	private Node(T data, Node<T> nextNode) {
+		this.data = data;
+		this.nextNode = nextNode;
+	}
+		
+	public T getData() {
+		return data;
 	}
 	
-	private class listIteratorImp implements Iterator<T>{
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+	
+	@SuppressWarnings("unused")
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	}
+	
+    private static class ListIteratorImp<T> implements Iterator<T> {
 
-		private Node curr;
+	private Node<T> currentNode;
+    SinglyLinkedList<T> list;
+    private volatile int iteratorCounter = 0;
+    
+	private ListIteratorImp(SinglyLinkedList<T> list) {
+		currentNode = list.head;
+		this.list = list;
+		iteratorCounter = list.listCounter;
+	}
+	@Override
+	public boolean hasNext() {
+
+		if (iteratorCounter != list.listCounter) {
+			throw new ConcurrentModificationException("ERROR: Data Structre have been modified");
+        }
 		
-		@Override
-		public boolean hasNext() {
-			return true;
-		}
-		
-		@Override
-		public T next() {
-			return null;
-		}
+		return(currentNode.getNextNode() != null );
 	}
 	
 	@Override
-	public Iterator<T> iterator() {
-		return null;
-	}
-	
-	public void pushFront(T data) {
+	public T next() {
 		
-	}
-	public T popFront() {
-		return null;
-	}
-	public int size() {
-		return 0;
-	}
-	public Iterator<T> find(T data) {
-		return null;
-	}
-	public boolean isEmpty() {
-		return true;
-	}
-	public static <E> SinglyLinkedList<E> Merge(SinglyLinkedList<E> list1, SinglyLinkedList<E> list2) {
-		return null;
+		if (iteratorCounter != list.listCounter) {
+			throw new ConcurrentModificationException("ERROR: Data Structre have been modified");
+        }
 		
-	}
-	public static <E> SinglyLinkedList<E> newReverse(SinglyLinkedList<E> list) {
-	
-		return null;
-	}
-	public static <E> void Print(SinglyLinkedList<E> list) {
+		if (list.isEmpty()) {
+			throw new NullPointerException("list is empty");
+        }
 		
+		T data = currentNode.getData();
+		currentNode = currentNode.getNextNode();	
+	
+		return data;
 	}
-	
-	
-
-
 }
 
+	@Override
+	public Iterator<T> iterator() {
+		return new ListIteratorImp<T>(this); 
+	}
 
+	public void pushFront(T data) {
+		++listCounter;
+		Node<T> newNode = new Node<T>(data, head);
+		head = newNode;
+	}
+
+	public T popFront() {
+	++listCounter;
+	
+	if (isEmpty()) {
+			throw new ConcurrentModificationException("Can't pop from Empty List");
+	}
+	
+	T data = head.getData();
+	head = head.getNextNode();
+	
+	return data;
+}
+
+	public int size() {
+	int size = 0;
+	for (@SuppressWarnings("unused") T item : this) {
+		++size;
+	}
+		return size;
+	}
+	
+	public Iterator<T> find(T data) {
+		Iterator<T> iter = iterator();
+		
+        for(T e : this) {
+        	if (e.equals(data)) {
+                return iter;
+            }
+        	
+            iter.next();
+        }
+		
+		return null;
+	}
+	
+	public boolean isEmpty() {
+		return (head.getNextNode() == null);
+	}
+	public static <E> SinglyLinkedList<E> Merge(SinglyLinkedList<E> list1, SinglyLinkedList<E> list2) {
+        SinglyLinkedList<E> newSinglyList = new SinglyLinkedList<>();
+        
+        for(E element: list1) {
+        	newSinglyList.pushFront(element);
+        }
+        
+        for(E element: list2) {
+        	newSinglyList.pushFront(element);
+        }
+        return newSinglyList;
+		
+	}
+	
+	public static <E> SinglyLinkedList<E> newReverse(SinglyLinkedList<E> list) {
+		SinglyLinkedList<E> reversList = new  SinglyLinkedList<>();
+	
+		for (E item : list) {
+			reversList.pushFront(item);
+		}
+				
+		return reversList;
+	}
+	
+	public static <E> void Print(SinglyLinkedList<E> list) {
+
+		for (E item : list) {
+			System.out.println(item.toString());
+		}
+		
+	}
+	
+}
---
+++ fs/projects/src/il/co/ilrd/GenericSLL/Test.java
@@ -0,0 +1,74 @@
+package il.co.ilrd.GenericSLL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		
+		SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
+		SinglyLinkedList<Integer> list2 = new SinglyLinkedList<>();
+		SinglyLinkedList<Integer> list3 = new SinglyLinkedList<>();
+		SinglyLinkedList<Integer> list4 = new SinglyLinkedList<>();
+		
+		assertTrue(list1.isEmpty());
+		assertEquals(0, list1.size());
+		
+		list1.pushFront(1);
+		assertEquals(1, list1.size());
+		assertFalse(list1.isEmpty());
+		SinglyLinkedList.Print(list1);
+		System.out.println();
+		list1.pushFront(2);
+		list1.pushFront(3);
+		list1.pushFront(4);
+		assertEquals(4, list1.size());
+		assertFalse(list1.isEmpty());
+		SinglyLinkedList.Print(list1);
+		System.out.println();
+		Integer i = 4;	
+		assertEquals(i, list1.popFront());
+		SinglyLinkedList.Print(list1);
+		System.out.println();
+		SinglyLinkedList<Integer> revers = SinglyLinkedList.newReverse(list1);
+		SinglyLinkedList.Print(revers);
+		System.out.println();
+		
+		list2.pushFront(8);
+		list2.pushFront(9);
+		list2.pushFront(10);
+		list2.pushFront(11);
+		
+		SinglyLinkedList<Integer> mergelist = SinglyLinkedList.Merge(list1, list2);
+		SinglyLinkedList.Print(mergelist);
+		System.out.println();
+		assertEquals(7, mergelist.size());
+		assertEquals((Integer)11, mergelist.find(11).next());
+		
+
+		try {
+			list3.pushFront(67);
+			Iterator<Integer> iter = list3.iterator();
+			iter.next();
+			list3.pushFront(60);
+			iter.next();
+
+			System.out.println("Shouldn't get here");
+			iter.next();
+			
+		} catch(Exception e) {
+			assertEquals(e.getMessage(), "ERROR: Data Structre have been modified");
+		}
+		
+		try {
+			list4.popFront();
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Can't pop from Empty List");
+		}
+	
+	}	
+
+}
